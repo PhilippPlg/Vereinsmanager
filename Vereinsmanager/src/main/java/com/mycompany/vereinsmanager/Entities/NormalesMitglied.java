7bd9a74fb3feff0d9259c86454c5025361045271
Tim Altenburg <tim.altenburg@gmx.de>
2021-02-08 01:09:12 +0100

    NormalesMitglied javadoc

--- Vereinsmanager/src/main/java/com/mycompany/vereinsmanager/Entities/NormalesMitglied.java
+++ Vereinsmanager/src/main/java/com/mycompany/vereinsmanager/Entities/NormalesMitglied.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.vereinsmanager.Entities;
 
 import java.util.Date;
@@ -12,21 +7,43 @@ import java.util.Date;
  * @author masia
  */
 public class NormalesMitglied extends Mitglied {
-    
+    /**
+     * zu zahlender Gesamtbetrag
+     */
     private double zuZahlenderBetrag;
 
+    /**
+     * Erstellt ein neues normales Mitglied
+     */
     public NormalesMitglied() {
     }
-    
+    /**
+     * Erstellt ein normales Mitglied
+     * @param vorname Vorname des Mitglieds
+     * @param nachname Nachname des Mitglieds
+     * @param straße Straße des Mitglieds
+     * @param PLZ Postleitzahl des Mitglieds
+     * @param ort Ort des Mitglieds
+     * @param geburtsDatum Geburtsdatum des Mitglieds
+     * @param email Email-Adresse des Mitglieds
+     * @param telefonNr Telefonnummer des Mitglieds
+     * @param beitrag zu zahlender Beitrag
+     */
     public NormalesMitglied( String vorname, String nachname, String straße, String PLZ, String ort, Date geburtsDatum, String email, String telefonNr, Double beitrag ) {
         super( vorname, nachname, straße, PLZ, ort, geburtsDatum, email, telefonNr);
         this.zuZahlenderBetrag = beitrag;
     }
-
+    /**
+     * Gibt den offenen Zahlungsbetrag zurück
+     * @return betrag
+     */
     public double getZuZahlenderBetrag() {
         return zuZahlenderBetrag;
     }
-
+    /**
+     * Setzt den zu zahlenden offenen Betrag auf den übergebenen Wert
+     * @param zuZahlenderBetrag 
+     */
     public void setZuZahlenderBetrag(double zuZahlenderBetrag) {
         this.zuZahlenderBetrag = zuZahlenderBetrag;
     }
